62ab98d142510ef7c38db99cf8d4ce775454a0a0
unknown <shubham>
2023-04-28 16:36:29 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Shubham from './Shubham';
+import Shubham2 from './Shubham2';
 
 function App() {
   return (
    <>
    <Shubham/>
-   
+   <Shubham2/>
    
    
    </>
---
+++ src/Shubham2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shubham2 = () => {
+  return (
+    <div>
+     shubham 
+    </div>
+  )
+}
+
+export default Shubham2
